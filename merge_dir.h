3dc7e2244b3088eb1ae72b965dc89f5b60f0cd84
20858559 <20858559@student.uwa.edu.au>
2012-11-01 17:53:22 +0800

    up2

---
+++ merge_dir.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   merge_dir.h
+ * Author: arnaudmoret
+ *
+ * Created on November 1, 2012, 4:22 PM
+ */
+
+#ifndef MERGE_DIR_H
+#define	MERGE_DIR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "file_headers.h"
+#include "globals.h"
+#include "sys_headers.h"
+#include "log.h"
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MERGE_DIR_H */
+
